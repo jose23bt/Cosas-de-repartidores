0557f6b0ec934b1d41f0a25c71bcfe3cb6de869c
jose23bt <jose.antonio23b@gmail.com>
2024-07-30 14:35:39 -0300

    mensaje

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ function HomePage() {
     return (
         <div>
             <h1>Qlq marico no hay nada aqui todavia</h1>
-            <p>ve de regreso al a pagos porque no hay mas nada :)</p>
+            <p>ve al simulador de pagos porque no hay mas nada :)</p>
         </div>
     );
 }
