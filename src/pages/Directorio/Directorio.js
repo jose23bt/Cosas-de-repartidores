ead1cfaa3210fde527e4ad9325f6669db2f34899
jose23bt <jose.antonio23b@gmail.com>
2024-08-01 14:46:08 -0300

    nuevo contacto

--- src/pages/Directorio/Directorio.js
+++ src/pages/Directorio/Directorio.js
@@ -22,16 +22,26 @@ const DirectoryItem = ({ category, contacts }) => (
 const Directorio = () => {
   const categories = {
     'Abogados': [
-      {
-        name: 'Edu',
-        address: 'Vélez Sársfield 2437',
-        phone: '11 5623-5039',
-        economicLevel: 4, // Nivel económico del 1 al 10, se representará con 6 símbolos de $
-        history: 'Todos hemos ido ahi, hace la chamba, te atiende bien pero ojo que puede fallar o robar jaja nahhh mentira pero pilas',
-        references: 'Su primer cliente, el samu. luego daniel se sintio celoso y lo desplazo y ahora es su amante y fan 1ro.'
-      }
+
     ],
-    'Mecánicos': [],
+    'Mecánicos': [
+        {
+            name: 'Edu',
+            address: 'Vélez Sársfield 2437, lanus',
+            phone: '11 5623-5039',
+            economicLevel: 4, // Nivel económico del 1 al 10, se representará con 6 símbolos de $
+            history: 'Todos hemos ido ahi, hace la chamba, te atiende bien pero ojo que puede fallar o robar jaja nahhh mentira pero pilas (no hace presupuestos si te chocan)',
+            references: 'Su primer cliente, el samu. luego daniel se sintio celoso y lo desplazo y ahora es su amante y fan 1ro.'
+          },
+        {
+            name: 'Mauricio',
+            address: 'Granaderos entre 1200 a 1300 al lado de un chino',
+            phone: '11 6583-8203',
+            economicLevel: 2, // Nivel económico del 1 al 10, se representará con 6 símbolos de $
+            history: 'Yube lleva su moto alli, odia a edu, dice q trabaja bien pero le da miedo recomendarlo, Eze obedece a yube y tambien la lleva ahi',
+            references: 'aun que yube no quiere ella es la unica referencia y dice que es el mejor mecanico de todos los tiempos'
+          }
+    ],
     'Tiendas': [],
     'Otros': [],
   };
